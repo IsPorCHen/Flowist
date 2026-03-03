89fbae45ee53f04fc8fbe91bcf94548d49b8fce7
IsPorCHen <fazy2019@gmail.com>
2026-03-04 03:58:42 +0500

    feat: add working echo bot with Docker

---
+++ src/Presentation/TelegramBot/Flowist.TelegramBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
+
+if (string.IsNullOrEmpty(token))
+{
+    Console.WriteLine("Token will not find");
+}
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running.");
+await Task.Delay(-1, cts.Token);
+
+// method that handle messages received by the bot:
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) return;	// we only handle Text messages here
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    // let's echo back received text in the chat
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
