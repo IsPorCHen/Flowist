dafa1b9bea8cb202c2a759f138a24fe2dceff724
IsPorCHen <fazy2019@gmail.com>
2026-03-09 20:36:04 +0500

    feat: implement environment-based configuration

---
+++ src/Infrastructure/Flowist.Infrastructure/Config/EnvBotConfig.cs
@@ -0,0 +1,9 @@
+using Flowist.Core.Interfaces;
+
+namespace Flowist.Infrastructure.Config;
+
+public class EnvBotConfig: IBotConfig
+{
+    public string TelegramToken => Environment.GetEnvironmentVariable("TELEGRAM BOT TOKEN")
+        ?? throw new InvalidOperationException("Telegram bot token is not set in environment variables.");
+}
